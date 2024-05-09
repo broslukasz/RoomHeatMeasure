92a560144b6a8cb637ff72a5e101936dc40c2743
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-05-09 15:52:14 +0200

    directional light added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,22 +4,25 @@ import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import Room from "./Room/room";
 import { useRef } from "react";
-import { PointLightHelper} from "three";
+import { DirectionalLightHelper, PointLightHelper} from "three";
 
 export const Experience = () => {
   const { performanceVisible } = useControls({ 
     performanceVisible: false,
   });
   const lightHelper = useRef(null);
+  const pointLightHelper = useRef(null);
 
-  useHelper(lightHelper, PointLightHelper )
+  useHelper(lightHelper, DirectionalLightHelper )
+  useHelper(pointLightHelper, PointLightHelper )
   
   return (
     <>
       {/* <ambientLight /> */}
       { performanceVisible && <Perf position={'top-left'}></Perf>}
       <OrbitControls />
-      <pointLight castShadow ref={lightHelper} position={[0, 4.5, 0]} intensity={1}></pointLight>
+      <directionalLight castShadow ref={lightHelper} position={[0, 4.5, 0]} intensity={0.5}></directionalLight>
+      <pointLight ref={pointLightHelper} position={[0, 4.5, 0]} intensity={0.5}></pointLight>
       <Room />
     </>
   );
