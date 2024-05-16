499404932e19a912a12ec8b66b3386e7c611fa15
Lukasz Bros <lukasz.bros.sp@gmail.com>
2024-05-16 12:47:00 +0200

    measurement added to the parent

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,7 +3,7 @@ import { OrbitControls, useHelper } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import Room from "./Room/room";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { DirectionalLightHelper, PointLightHelper} from "three";
 
 export const Experience = () => {
@@ -12,6 +12,13 @@ export const Experience = () => {
   });
   const lightHelper = useRef(null);
   const pointLightHelper = useRef(null);
+  const [measurement, setMeasurement] = useState(null)
+
+  const onRoomClick = ($event: any) => {
+    const {x, y, z} = $event.intersections[0].point;
+    const yTransition = 1;
+    setMeasurement([x, y + yTransition, z]);
+  }
 
   useHelper(lightHelper, DirectionalLightHelper )
   useHelper(pointLightHelper, PointLightHelper )
@@ -23,7 +30,12 @@ export const Experience = () => {
       <OrbitControls />
       <directionalLight castShadow ref={lightHelper} position={[0, 4.5, 0]} intensity={0.5}></directionalLight>
       <pointLight ref={pointLightHelper} position={[0, 4.5, 0]} intensity={0.5}></pointLight>
-      <Room />
+      <Room onClick={onRoomClick} />
+
+      {measurement && <mesh castShadow position={measurement}>
+        <sphereGeometry args={[ 0.1, 32, 16]} />
+        <meshStandardMaterial color={0xffff00} />
+      </mesh>}
     </>
   );
 };
--- src/components/Room/room.tsx
+++ src/components/Room/room.tsx
@@ -1,12 +1,9 @@
 import { useLoader } from "@react-three/fiber";
-import { useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-export default function Room() {
+export default function Room({ onClick }) {
   const gltf = useLoader(GLTFLoader, 'src/assets/office/scene.gltf');
   const model = gltf.scene;
-
-  const [measurement, setMeasurement] = useState(null)
 
   model.traverse(o => {
     if(o.isMesh) {
@@ -14,17 +11,7 @@ export default function Room() {
     }
   })
 
-  const addMesh = ($event: any) => {
-    const {x, y, z} = $event.intersections[0].point;
-    const yTransition = 1;
-    setMeasurement([x, y + yTransition, z]);
-  }
-
   return <>
-    <primitive castShadow receiveShadow onClick={($event) => addMesh($event)} object={model} />
-    {measurement && <mesh castShadow position={measurement}>
-      <sphereGeometry args={[ 0.1, 32, 16]} />
-      <meshStandardMaterial color={0xffff00} />
-    </mesh>}
+    <primitive castShadow receiveShadow onClick={onClick} object={model} />
   </>
 }
